07422e4abb4e08267768db1586ea0988e3f60f13
Minish144 <varlamow.col@yahoo.com>
2019-11-04 22:31:39 +0300

    удалил лишний файл screenshot.png обновил main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import time
 class screen:   
     weight = 1920
     height = 1080
-
+    
 # создание скриншота
 image = pyautogui.screenshot(region=(screen.weight/2 - 100, screen.height/2 - 150, 200, 200))
 image = cv2.cvtColor(numpy.array(image), 0)
@@ -22,7 +22,6 @@ template_coordinates = cv2.matchTemplate(image, template, cv2.TM_CCOEFF_NORMED)
 if (abs(cv2.minMaxLoc(template_coordinates)[3][0] - cv2.minMaxLoc(template_coordinates)[3][1])<=10):
     print("Time to fish!")
     pyautogui.press('g')
-    time.sleep(0.4)
 else:
     print("Not time yet!")
 
