ce68efff6f969a687f1f08366badda92afce74e0
Minish144 <varlamow.col@yahoo.com>
2019-11-04 21:15:24 +0300

    добавил шаблон восклицательного знака и поменял координаты для поиска

--- main.py
+++ main.py
@@ -9,12 +9,12 @@ class screen:
     height = 1080
 
 #создание скриншот
-image = pyautogui.screenshot(region=(screen.weight/3, screen.height/3, 300, 300))
+image = pyautogui.screenshot(region=(screen.weight/2 - 150, screen.height/2 - 150, 300, 300))
 image = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
-cv2.imwrite("pic.png", image)
+cv2.imwrite("screenshot.png", image)
 
 #запись скриншота
-image = cv2.imread("pic.png")
-cv2.imshow("pic.png", image)
+image = cv2.imread("screenshot.png")
+cv2.imshow("screenshot.png", image)
 
 cv2.waitKey(0)
