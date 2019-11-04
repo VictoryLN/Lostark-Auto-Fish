27d7b406d9c11d565c7779870a853c31500eb60e
Minish144 <varlamow.col@yahoo.com>
2019-11-04 22:28:44 +0300

    теперь скриншот не сохраняется на диск, что экономит время

--- main.py
+++ main.py
@@ -2,28 +2,27 @@ import numpy
 import pyautogui
 import imutils
 import cv2
+import time
 
-# разрешение экрана (customizable)
+# разрешение экрана
 class screen:   
     weight = 1920
     height = 1080
 
 # создание скриншота
 image = pyautogui.screenshot(region=(screen.weight/2 - 100, screen.height/2 - 150, 200, 200))
-image = cv2.cvtColor(numpy.array(image), cv2.COLOR_RGB2BGR)
-cv2.imwrite("screenshot.png", image)
+image = cv2.cvtColor(numpy.array(image), 0)
+image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-# запись скриншота
-image = cv2.imread('screenshot.png', 0)
-#cv2.imshow("screenshot", image)
-
-# шаблон
+# картиночки
 template = cv2.imread("template.png", 0)
-#cv2.imshow("template", template)
 
 # поиск шаблона на скрине
 template_coordinates = cv2.matchTemplate(image, template, cv2.TM_CCOEFF_NORMED)
-if (abs(cv2.minMaxLoc(template_coordinates)[3][0] - cv2.minMaxLoc(template_coordinates)[3][1])<=20):
-    print("Time to fish")
+if (abs(cv2.minMaxLoc(template_coordinates)[3][0] - cv2.minMaxLoc(template_coordinates)[3][1])<=10):
+    print("Time to fish!")
+    pyautogui.press('g')
+    time.sleep(0.4)
 else:
-    print("not time yet")
+    print("Not time yet!")
+
