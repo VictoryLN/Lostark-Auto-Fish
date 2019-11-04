fabc27b08c0d53b6d056d9e44428d43c1ac65a40
Minish144 <varlamow.col@yahoo.com>
2019-11-04 22:44:15 +0300

    сделал возможность закидывать рыбу и сделал цикл

--- main.py
+++ main.py
@@ -8,20 +8,27 @@ import time
 class screen:   
     weight = 1920
     height = 1080
-    
-# создание скриншота
-image = pyautogui.screenshot(region=(screen.weight/2 - 100, screen.height/2 - 150, 200, 200))
-image = cv2.cvtColor(numpy.array(image), 0)
-image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+flag = False
 
-# картиночки
-template = cv2.imread("template.png", 0)
+while(1)
+    if flag == True:
+        pyautogui.press('w')
+        time.sleep(5)
+    # создание скриншота
+    image = pyautogui.screenshot(region=(screen.weight/2 - 100, screen.height/2 - 150, 200, 200))
+    image = cv2.cvtColor(numpy.array(image), 0)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-# поиск шаблона на скрине
-template_coordinates = cv2.matchTemplate(image, template, cv2.TM_CCOEFF_NORMED)
-if (abs(cv2.minMaxLoc(template_coordinates)[3][0] - cv2.minMaxLoc(template_coordinates)[3][1])<=10):
-    print("Time to fish!")
-    pyautogui.press('g')
-else:
-    print("Not time yet!")
+    # картиночки
+    template = cv2.imread("template.png", 0)
 
+    # поиск шаблона на скрине
+    template_coordinates = cv2.matchTemplate(image, template, cv2.TM_CCOEFF_NORMED)
+    if (abs(cv2.minMaxLoc(template_coordinates)[3][0] - cv2.minMaxLoc(template_coordinates)[3][1])<=10):
+        print("Time to fish!")
+        pyautogui.press('w')
+        flag = True
+        time.sleep(7)
+    else:
+        print("Not time yet!")
+        flag = False
