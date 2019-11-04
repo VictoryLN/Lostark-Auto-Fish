e949bfe9e73fac70664c58e814726f180563656c
Minish144 <varlamow.col@yahoo.com>
2019-11-04 22:09:16 +0300

    теперь код знает есть на скрине восклицательный знак или нет

--- main.py
+++ main.py
@@ -1,20 +1,29 @@
-import numpy as np
+import numpy
 import pyautogui
 import imutils
 import cv2
 
-#разрешение экрана (customizable)
+# разрешение экрана (customizable)
 class screen:   
     weight = 1920
     height = 1080
 
-#создание скриншот
-image = pyautogui.screenshot(region=(screen.weight/2 - 150, screen.height/2 - 150, 300, 300))
-image = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
+# создание скриншота
+image = pyautogui.screenshot(region=(screen.weight/2 - 100, screen.height/2 - 150, 200, 200))
+image = cv2.cvtColor(numpy.array(image), cv2.COLOR_RGB2BGR)
 cv2.imwrite("screenshot.png", image)
 
-#запись скриншота
-image = cv2.imread("screenshot.png")
-cv2.imshow("screenshot.png", image)
+# запись скриншота
+image = cv2.imread('screenshot.png', 0)
+#cv2.imshow("screenshot", image)
 
-cv2.waitKey(0)
+# шаблон
+template = cv2.imread("template.png", 0)
+#cv2.imshow("template", template)
+
+# поиск шаблона на скрине
+template_coordinates = cv2.matchTemplate(image, template, cv2.TM_CCOEFF_NORMED)
+if (abs(cv2.minMaxLoc(template_coordinates)[3][0] - cv2.minMaxLoc(template_coordinates)[3][1])<=20):
+    print("Time to fish")
+else:
+    print("not time yet")
