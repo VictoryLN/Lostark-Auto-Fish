a7735a90da9c4c60d29bf231c4ecc3f0f3e7ff32
Nikolay <varlamow.col@yahoo.com>
2019-11-05 00:27:50 +0300

    Оптимизировал код

--- main.py
+++ main.py
@@ -10,21 +10,24 @@ class screen:
     height = 1080
 flag = False
 
+# шаблон
+template = cv2.imread("template.png", 0)
+
 while(1):
     if flag == True:
         pyautogui.press('w')
         time.sleep(5)
+
     # создание скриншота
     image = pyautogui.screenshot(region=(screen.weight/2 - 100, screen.height/2 - 150, 200, 200))
     image = cv2.cvtColor(numpy.array(image), 0)
     image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-    # картиночки
-    template = cv2.imread("template.png", 0)
-
     # поиск шаблона на скрине
     template_coordinates = cv2.matchTemplate(image, template, cv2.TM_CCOEFF_NORMED)
-    if (abs(cv2.minMaxLoc(template_coordinates)[3][0] - cv2.minMaxLoc(template_coordinates)[3][1])<=10):
+
+    # исходя из результатов поиска либо нажимается W либо ничего не происходит и цикл идет по новой
+    if (abs(cv2.minMaxLoc(template_coordinates)[3][0] - cv2.minMaxLoc(template_coordinates)[3][1])<=5):
         print(strftime("%H:%M:%S", gmtime()), "Time to fish!")
         pyautogui.press('w')
         flag = True
