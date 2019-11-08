76eb9dae14d7dcc5ae76f6104a4c754555e5dc06
Minish144 <varlamow.col@yahoo.com>
2019-11-08 15:04:30 +0300

    Изменил эмуляцию кнопок, пофиксил пару багов

--- main.py
+++ main.py
@@ -4,20 +4,30 @@ import cv2
 import time
 from time import gmtime, strftime
 import random
+import win32gui
 
 # разрешение экрана
 class screen:   
     weight = 1920
     height = 1080
-flag = False
+flag = "pulled"
 
-# шаблон
+# шаблоны
 template = cv2.imread("template.png", 0)
+poplavok = cv2.imread("poplavok.png", 0)
+
+print(strftime("%H:%M:%S", gmtime()), "starting a bot")
+time.sleep(5)
 
 while(1):
-    if flag == True:
-        pyautogui.press('w')
-        time.sleep( random.uniform(4.5, 6.5))
+    if flag == "pulled":
+        print(strftime("%H:%M:%S", gmtime()), "throwing a fishing rod [1]")
+        pyautogui.keyDown('w')
+        time.sleep( random.uniform( 0.05, 0.1 ))
+        pyautogui.keyUp('w')
+        flag = "thrown"
+        time.sleep( random.uniform(4.5, 6.5))        
+        
 
     # создание скриншота
     image = pyautogui.screenshot(region=(screen.weight/2 - 100, screen.height/2 - 150, 200, 200))
@@ -28,13 +38,30 @@ while(1):
     template_coordinates = cv2.matchTemplate(image, template, cv2.TM_CCOEFF_NORMED)
     loc = numpy.where( template_coordinates >= 0.7)
 
+
     # исходя из результатов поиска либо нажwимается W либо ничего не происходит и цикл идет по новой
-    if len(loc[0]) > 0:    
-        print(strftime("%H:%M:%S", gmtime()), "Time to fish!", loc[0])
-        time.sleep(random.uniform(0.2, 1.0))
-        pyautogui.press('w')
-        flag = True
-        time.sleep(random.uniform(5.5, 7.5))
-    else:
-        print(strftime("%H:%M:%S", gmtime()), "Not time yet!", loc[0])
-        flag = False
+    if len(loc[0]) > 0:
+        if flag == "thrown":  
+            print(strftime("%H:%M:%S", gmtime()), "Time to fish!")
+            time.sleep(random.uniform(0.2, 1.0))
+            pyautogui.keyDown('w')
+            time.sleep( random.uniform( 0.05, 0.1 ))
+            pyautogui.keyUp('w')
+            flag = "pulled"
+            time.sleep(random.uniform(5.5, 7.5))
+
+    poplavok_coordinates = cv2.matchTemplate(image, poplavok, cv2.TM_CCOEFF_NORMED)
+    poplavok_loc = numpy.where( poplavok_coordinates >= 0.7)
+    
+    if len(poplavok_loc[0]) == 0 and flag == "pulled":
+        print(strftime("%H:%M:%S", gmtime()), "throwing a fishing rod [2]")
+        pyautogui.keyDown('w')
+        time.sleep( random.uniform( 0.05, 0.1 ))
+        pyautogui.keyUp('w')
+        flag = "thrown"
+        time.sleep( random.uniform(4.5, 6.5))
+
+    print(strftime("%H:%M:%S", gmtime()), "Not time yet!")
+            
+            
+    
